febd524aa3019308b0bd517ba2f70bf089bc6e69
Sachin Tharaka <60689075+Sachin-Tharaka@users.noreply.github.com>
2020-11-15 17:22:21 +0530

    Create Contactlist.java

---
+++ Contactlist.java
@@ -0,0 +1,24 @@
+package javaapplication4;
+import java.util.ArrayList;
+public class ContactList {
+    ArrayList<String> con = new ArrayList<String>();
+    
+    public static void addContact(){
+        
+    }
+    public static void  deleteContact(){
+        
+    }
+    public static void editContact(){
+        
+    }
+    public static void displayContacts(){
+        
+    }
+    public static void loadContacts(){
+        
+    }public static void  saveContacts(){
+        
+    }
+    
+}
